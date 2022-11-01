9a95f115bbbe17c3b0c367d7baf3e3c3dcbe74cd
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2022-11-01 14:44:46 +1300

    Add files via upload

--- 01_bits_calculator.py
+++ 01_bits_calculator.py
@@ -2,19 +2,86 @@
 
 # puts symbols at start and end of text (for emphasis)
 
-def statement_generator(text, decoration): 
+def statement_generator(text, decoration, lines) :
+    middle = "{} {} {}".format(decoration*3, text, decoration*3)
+    top_bottom = decoration * len(middle)
 
-    # make string with 5 characters 
-    ends = decoration * 5 
+    if lines == 3:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+    else:
+        print(middle)
 
-    # add decoration to start and end of statement 
-    statement = "{} {} {}".format(ends, text, ends)
+    return None
 
-    print() 
-    print(statement) 
-    print() 
+    
+    statement_generator("Bits Calculator", "-")
 
-    return ""
+# checks users enter text / image or integet
+def user_choice(): 
+   
+    # list of valid responses
+    
+    text_ok = ["t", "text", "txt"]
+    interger_ok = ["in", "int", "integer"]
+    image_ok = ["image", "img","p"]
+    
+    # checks if users choice is vaiable
+
+    valid = False 
+    while not valid: 
+        print()
+        response = input("file type (interger / text / image): ").lower()  
+        
+        if response in text_ok :
+            return "text" 
+
+        elif response in interger_ok: 
+            return "integer" 
+        
+        elif response in image_ok:
+            return "image" 
+
+        elif response == "i": 
+            want_int = input("press <enter> for an interger, or any key, <enter> for image")
+            if want_int == "": 
+                return "integer" 
+            else:
+                return "image"
+
+        # error message 
+
+        else: 
+            print()
+            print("Please choose a valid file type!") 
+            print() 
+
+    data_type = user_choice() 
+    print()
+    print("you chose", data_type) 
+    print()
 
 # main routine goes here 
-statement_generator("look - stars", "*")
+
+statement_generator("bit calculator for intergers, text and images", "-", 3)
+
+# display instruction if user has never used the program before
+
+# loop to allopw for multiple calculations per second
+
+keep_going = ""
+while keep_going == "":
+    
+    # checks users choice is 'interger', 'text' or 'image' 
+    data_type = user_choice()
+    print(data_type)
+
+    keep_going = input("Press <enter> to keep going or any key to quit")
+    print()
+
+print("We are done") 
+print()
