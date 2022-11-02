616a90417057beb4249683dd043430f7cd037a4e
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2022-11-03 11:43:34 +1300

    Add files via upload

---
+++ 03_integer_checker.py
@@ -0,0 +1,11 @@
+# checks if integer is valid
+def num_checker(question): 
+    valid = False 
+    while not valid: 
+
+        error = "please choose a valid number" 
+
+        try: 
+            response = float(input(question)) 
+
+            if response 
