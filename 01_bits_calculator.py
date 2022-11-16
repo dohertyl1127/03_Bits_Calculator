35fa713f1c4583d677443e2b3962f9d80aa5e22b
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2022-11-17 11:50:05 +1300

    Add files via upload

--- 01_bits_calculator.py
+++ 01_bits_calculator.py
@@ -110,6 +110,26 @@ def num_check(question, low):
             print()
  
  
+def text_bits(): 
+
+    print() 
+    # ask user for a string 
+    var_text = input("enter your text: ") 
+
+    # calculate the number of bits (length of string *8) 
+    var_length = len(var_text) 
+    num_bits = 8 * var_length 
+
+    # output answer with working 
+    print() 
+    print("\'{}\' has {} characters ...".format(var_text, var_length))
+    print("# of bits is {} * 8".format(var_length)) 
+    print("we need {} bits to represent {}".format(num_bits, var_text)) 
+    print() 
+
+    return "" 
+
+
 # main routine goes here 
 
 statement_generator("bit calculator for intergers, text and images", "-", 3)
